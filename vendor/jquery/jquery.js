174c05c4f64a582c9bf0cf4bf6ece2ab61ac64a6
catarinacruz <catarina.m.c.2401@gmail.com>
2016-12-11 18:13:13 +0000

    JQuery

--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -95,120 +95,120 @@
 	    pushStack: function (elems) {
         
 		// Build a new jQuery matched element set
-		var ret = jQuery.merge( this.constructor(), elems );
+		    var ret = jQuery.merge(this.constructor(), elems);
 
 		// Add the old object onto the stack (as a reference)
-		ret.prevObject = this;
-		ret.context = this.context;
+            ret.prevObject = this;
+		    ret.context = this.context;
 
 		// Return the newly-formed element set
-		return ret;
-	},
+		    return ret;
+	    },
 
 	// Execute a callback for every element in the matched set.
-	each: function( callback ) {
-		return jQuery.each( this, callback );
-	},
+	    each: function (callback) {
+		    return jQuery.each(this, callback);
+	    },
 
-	map: function( callback ) {
-		return this.pushStack( jQuery.map( this, function( elem, i ) {
-			return callback.call( elem, i, elem );
-		} ) );
-	},
+	    map: function (callback) {
+		    return this.pushStack(jQuery.map(this, function (elem, i) {
+			    return callback.call(elem, i, elem);
+		    }));
+	    },
 
-	slice: function() {
-		return this.pushStack( slice.apply( this, arguments ) );
-	},
+	    slice: function () {
+		    return this.pushStack(slice.apply(this, arguments));
+	    },
 
-	first: function() {
-		return this.eq( 0 );
-	},
+	    first: function () {
+		    return this.eq(0);
+	    },
 
-	last: function() {
-		return this.eq( -1 );
-	},
+	    last: function () {
+		    return this.eq(-1);
+	    },
 
-	eq: function( i ) {
-		var len = this.length,
-			j = +i + ( i < 0 ? len : 0 );
-		return this.pushStack( j >= 0 && j < len ? [ this[ j ] ] : [] );
-	},
+	    eq: function (i) {
+            var len = this.length,
+                j = +i + (i < 0 ? len : 0);
+            return this.pushStack(j >= 0 && j < len ? [this[j] ] : []);
+        },
 
-	end: function() {
-		return this.prevObject || this.constructor();
-	},
+        end: function () {
+            return this.prevObject || this.constructor();
+        },
 
-	// For internal use only.
-	// Behaves like an Array's method, not like a jQuery method.
-	push: push,
-	sort: deletedIds.sort,
-	splice: deletedIds.splice
-};
+        // For internal use only.
+        // Behaves like an Array's method, not like a jQuery method.
+        push: push,
+        sort: deletedIds.sort,
+        splice: deletedIds.splice
+    };
 
-jQuery.extend = jQuery.fn.extend = function() {
-	var src, copyIsArray, copy, name, options, clone,
-		target = arguments[ 0 ] || {},
-		i = 1,
-		length = arguments.length,
-		deep = false;
+                                                                                                          jQuery.extend = jQuery.fn.extend = function () {
+	    var src, copyIsArray, copy, name, options, clone,
+            target = arguments[0] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
 
 	// Handle a deep copy situation
-	if ( typeof target === "boolean" ) {
-		deep = target;
+	    if (typeof target === "boolean") {
+		    deep = target;
 
 		// skip the boolean and the target
-		target = arguments[ i ] || {};
-		i++;
-	}
+		    target = arguments[i] || {};
+		    i = i + 1;
+        }
 
-	// Handle case when target is a string or something (possible in deep copy)
-	if ( typeof target !== "object" && !jQuery.isFunction( target ) ) {
-		target = {};
-	}
+        // Handle case when target is a string or something (possible in deep copy)
+        if (typeof target !== "object" && !jQuery.isFunction(target)) {
+            target = {};
+        }
 
-	// extend jQuery itself if only one argument is passed
-	if ( i === length ) {
-		target = this;
-		i--;
-	}
+        // extend jQuery itself if only one argument is passed
+        if (i === length) {
+            target = this;
+            i = i - 1;
+        }
 
-	for ( ; i < length; i++ ) {
+	    for (i; i < length; i = i + 1) {
 
 		// Only deal with non-null/undefined values
-		if ( ( options = arguments[ i ] ) != null ) {
+		    if ((options = arguments[i]) !== null) {
 
 			// Extend the base object
-			for ( name in options ) {
-				src = target[ name ];
-				copy = options[ name ];
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
 
-				// Prevent never-ending loop
-				if ( target === copy ) {
-					continue;
-				}
+                    // Prevent never-ending loop
+                    if (target === copy) {
+                        continue;
+                    }
 
-				// Recurse if we're merging plain objects or arrays
-				if ( deep && copy && ( jQuery.isPlainObject( copy ) ||
-					( copyIsArray = jQuery.isArray( copy ) ) ) ) {
+                    // Recurse if we're merging plain objects or arrays
+                    if (deep && copy && (jQuery.isPlainObject(copy) ||
+                        (copyIsArray = jQuery.isArray(copy)))) {
 
-					if ( copyIsArray ) {
-						copyIsArray = false;
-						clone = src && jQuery.isArray( src ) ? src : [];
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && jQuery.isArray(src) ? src : [];
 
-					} else {
-						clone = src && jQuery.isPlainObject( src ) ? src : {};
-					}
+                        } else {
+                            clone = src && jQuery.isPlainObject(src) ? src : {};
+                        }
 
-					// Never move original objects, clone them
-					target[ name ] = jQuery.extend( deep, clone, copy );
+                        // Never move original objects, clone them
+                        target[name] = jQuery.extend(deep, clone, copy);
 
-				// Don't bring in undefined values
-				} else if ( copy !== undefined ) {
-					target[ name ] = copy;
-				}
-			}
-		}
-	}
+                    // Don't bring in undefined values
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
 
 	// Return the modified object
 	return target;
