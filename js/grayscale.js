4e162917367b92ea1f4607807d3eab80a6c1da25
tavarest <tomasfbtavares@gmail.com>
2017-01-02 15:29:35 +0000

    css e js a funcionar

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,42 +5,42 @@
  */
 
 // jQuery to collapse the navbar on scroll
-function collapseNavbar() {"use strict";
+function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-    }
+}
 
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function () {"use strict";
-    $('a.page-scroll').bind('click', function (event) {
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    });
+});
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function () {"use strict";
+$('.navbar-collapse ul li a').click(function() {
     $(this).closest('.collapse').collapse('toggle');
-     });
+});
 
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function () {"use strict";
+google.maps.event.addDomListener(window, 'resize', function() {
     map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-     });
+});
 
-function init() {"use strict";
+function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
@@ -182,4 +182,4 @@ function init() {"use strict";
         map: map,
         icon: image
     });
-     }
+}
--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -12,8 +12,9 @@
  * Date: 2016-05-20T17:17Z
  */
 
-(function (global, factory) {"use strict";
-	if (typeof module === "object" && typeof module.exports === "object") {
+(function( global, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
 		// For CommonJS and CommonJS-like environments where a proper `window`
 		// is present, execute the factory and get jQuery.
 		// For environments that do not have a `window` with a `document`
@@ -22,193 +23,219 @@
 		// e.g. var jQuery = require("jquery")(window);
 		// See ticket #14549 for more info.
 		module.exports = global.document ?
-                factory(global, true) :
-			     function (w) {
-				    if (!w.document) {
-					    throw new Error("jQuery requires a window with a document");
-				    }
-				    return factory(w);
-                };
+			factory( global, true ) :
+			function( w ) {
+				if ( !w.document ) {
+					throw new Error( "jQuery requires a window with a document" );
+				}
+				return factory( w );
+			};
 	} else {
-		factory(global);
+		factory( global );
 	}
 
 // Pass this if window is not defined yet
-                            }(typeof window !== "undefined" ? window : this, function (window, noGlobal) {"use strict";
+}(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
 
 // Support: Firefox 18+
 // Can't be in strict mode, several libs including ASP.NET trace
 // the stack via arguments.caller.callee and Firefox dies if
 // you try to trace through "use strict" call chains. (#13335)
 //"use strict";
-                                                                                                          var deletedIds = [], document = window.document, slice = deletedIds.slice, concat = deletedIds.concat, push = deletedIds.push, indexOf = deletedIds.indexOf, class2type = {}, toString = class2type.toString, hasOwn = class2type.hasOwnProperty, support = {},	version = "1.12.4";
+var deletedIds = [];
+
+var document = window.document;
+
+var slice = deletedIds.slice;
+
+var concat = deletedIds.concat;
+
+var push = deletedIds.push;
+
+var indexOf = deletedIds.indexOf;
+
+var class2type = {};
+
+var toString = class2type.toString;
+
+var hasOwn = class2type.hasOwnProperty;
+
+var support = {};
+
+
+
+var
+	version = "1.12.4",
 
 	// Define a local copy of jQuery
-	jQuery = function (selector, context) {
+	jQuery = function( selector, context ) {
+
 		// The jQuery object is actually just the init constructor 'enhanced'
 		// Need init if jQuery is called (just allow error to be thrown if not included)
-		return new jQuery.fn.init(selector, context);
-	};
+		return new jQuery.fn.init( selector, context );
+	},
 
 	// Support: Android<4.1, IE<9
 	// Make sure we trim BOM and NBSP
-	rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+	rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,
 
 	// Matches dashed string for camelizing
-	rmsPrefix = /^-ms-/;
-	rdashAlpha = /-([\da-z])/gi;
+	rmsPrefix = /^-ms-/,
+	rdashAlpha = /-([\da-z])/gi,
 
 	// Used by jQuery.camelCase as callback to replace()
-	fcamelCase = function (all, letter) {
+	fcamelCase = function( all, letter ) {
 		return letter.toUpperCase();
 	};
 
-                                                                                                          jQuery.fn = jQuery.prototype = {
+jQuery.fn = jQuery.prototype = {
+
 	// The current version of jQuery being used
-	    jquery: version,
-        constructor: jQuery,
+	jquery: version,
+
+	constructor: jQuery,
 
 	// Start with an empty selector
-	    selector: "",
+	selector: "",
 
 	// The default length of a jQuery object is 0
-	    length: 0,
+	length: 0,
 
-	    toArray: function () {
-		    return slice.call(this);
-	    },
+	toArray: function() {
+		return slice.call( this );
+	},
 
 	// Get the Nth element in the matched element set OR
 	// Get the whole matched element set as a clean array
-	    get: function (num) {
-		    return num !== null ?
+	get: function( num ) {
+		return num != null ?
 
 			// Return just the one element from the set
-			         (num < 0 ? this[num + this.length] : this[num]) :
+			( num < 0 ? this[ num + this.length ] : this[ num ] ) :
 
 			// Return all the elements in a clean array
-			         slice.call(this);
-	    },
+			slice.call( this );
+	},
 
 	// Take an array of elements and push it onto the stack
 	// (returning the new matched element set)
-	    pushStack: function (elems) {
-        
+	pushStack: function( elems ) {
+
 		// Build a new jQuery matched element set
-		    var ret = jQuery.merge(this.constructor(), elems);
+		var ret = jQuery.merge( this.constructor(), elems );
 
 		// Add the old object onto the stack (as a reference)
-            ret.prevObject = this;
-		    ret.context = this.context;
+		ret.prevObject = this;
+		ret.context = this.context;
 
 		// Return the newly-formed element set
-		    return ret;
-	    },
+		return ret;
+	},
 
 	// Execute a callback for every element in the matched set.
-	    each: function (callback) {
-		    return jQuery.each(this, callback);
-	    },
+	each: function( callback ) {
+		return jQuery.each( this, callback );
+	},
 
-	    map: function (callback) {
-		    return this.pushStack(jQuery.map(this, function (elem, i) {
-			    return callback.call(elem, i, elem);
-		    }));
-	    },
+	map: function( callback ) {
+		return this.pushStack( jQuery.map( this, function( elem, i ) {
+			return callback.call( elem, i, elem );
+		} ) );
+	},
 
-	    slice: function () {
-		    return this.pushStack(slice.apply(this, arguments));
-	    },
+	slice: function() {
+		return this.pushStack( slice.apply( this, arguments ) );
+	},
 
-	    first: function () {
-		    return this.eq(0);
-	    },
+	first: function() {
+		return this.eq( 0 );
+	},
 
-	    last: function () {
-		    return this.eq(-1);
-	    },
+	last: function() {
+		return this.eq( -1 );
+	},
 
-	    eq: function (i) {
-            var len = this.length,
-                j = +i + (i < 0 ? len : 0);
-            return this.pushStack(j >= 0 && j < len ? [this[j] ] : []);
-        },
+	eq: function( i ) {
+		var len = this.length,
+			j = +i + ( i < 0 ? len : 0 );
+		return this.pushStack( j >= 0 && j < len ? [ this[ j ] ] : [] );
+	},
 
-        end: function () {
-            return this.prevObject || this.constructor();
-        },
+	end: function() {
+		return this.prevObject || this.constructor();
+	},
 
-        // For internal use only.
-        // Behaves like an Array's method, not like a jQuery method.
-        push: push,
-        sort: deletedIds.sort,
-        splice: deletedIds.splice
-    };
+	// For internal use only.
+	// Behaves like an Array's method, not like a jQuery method.
+	push: push,
+	sort: deletedIds.sort,
+	splice: deletedIds.splice
+};
 
-                                                                                                          jQuery.extend = jQuery.fn.extend = function () {
-	    var src, copyIsArray, copy, name, options, clone,
-            target = arguments[0] || {},
-            i = 1,
-            length = arguments.length,
-            deep = false;
+jQuery.extend = jQuery.fn.extend = function() {
+	var src, copyIsArray, copy, name, options, clone,
+		target = arguments[ 0 ] || {},
+		i = 1,
+		length = arguments.length,
+		deep = false;
 
 	// Handle a deep copy situation
-	    if (typeof target === "boolean") {
-		    deep = target;
+	if ( typeof target === "boolean" ) {
+		deep = target;
 
 		// skip the boolean and the target
-		    target = arguments[i] || {};
-		    i = i + 1;
-        }
+		target = arguments[ i ] || {};
+		i++;
+	}
 
-        // Handle case when target is a string or something (possible in deep copy)
-        if (typeof target !== "object" && !jQuery.isFunction(target)) {
-            target = {};
-        }
+	// Handle case when target is a string or something (possible in deep copy)
+	if ( typeof target !== "object" && !jQuery.isFunction( target ) ) {
+		target = {};
+	}
 
-        // extend jQuery itself if only one argument is passed
-        if (i === length) {
-            target = this;
-            i = i - 1;
-        }
+	// extend jQuery itself if only one argument is passed
+	if ( i === length ) {
+		target = this;
+		i--;
+	}
 
-	    for (i; i < length; i = i + 1) {
+	for ( ; i < length; i++ ) {
 
 		// Only deal with non-null/undefined values
-		    if ((options = arguments[i]) !== null) {
+		if ( ( options = arguments[ i ] ) != null ) {
 
 			// Extend the base object
-                for (name in options) {
-                    src = target[name];
-                    copy = options[name];
+			for ( name in options ) {
+				src = target[ name ];
+				copy = options[ name ];
 
-                    // Prevent never-ending loop
-                    if (target === copy) {
-                        continue;
-                    }
+				// Prevent never-ending loop
+				if ( target === copy ) {
+					continue;
+				}
 
-                    // Recurse if we're merging plain objects or arrays
-                    if (deep && copy && (jQuery.isPlainObject(copy) ||
-                        (copyIsArray = jQuery.isArray(copy)))) {
+				// Recurse if we're merging plain objects or arrays
+				if ( deep && copy && ( jQuery.isPlainObject( copy ) ||
+					( copyIsArray = jQuery.isArray( copy ) ) ) ) {
 
-                        if (copyIsArray) {
-                            copyIsArray = false;
-                            clone = src && jQuery.isArray(src) ? src : [];
+					if ( copyIsArray ) {
+						copyIsArray = false;
+						clone = src && jQuery.isArray( src ) ? src : [];
 
-                        } else {
-                            clone = src && jQuery.isPlainObject(src) ? src : {};
-                        }
+					} else {
+						clone = src && jQuery.isPlainObject( src ) ? src : {};
+					}
 
-                        // Never move original objects, clone them
-                        target[name] = jQuery.extend(deep, clone, copy);
+					// Never move original objects, clone them
+					target[ name ] = jQuery.extend( deep, clone, copy );
 
-                    // Don't bring in undefined values
-                    } else if (copy !== undefined) {
-                        target[name] = copy;
-                    }
-                }
-            }
-        }
+				// Don't bring in undefined values
+				} else if ( copy !== undefined ) {
+					target[ name ] = copy;
+				}
+			}
+		}
+	}
 
 	// Return the modified object
 	return target;
